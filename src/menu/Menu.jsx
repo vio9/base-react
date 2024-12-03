6de1a6d2472e119503265473747f6fac698ffc24
violaine <e.violaine@yahoo.fr>
2024-12-03 16:58:51 +0100

    test materializecss, install sass

--- src/menu/Menu.jsx
+++ src/menu/Menu.jsx
@@ -2,10 +2,17 @@ import { Link } from "react-router-dom";
 
 function Menu(){
     return(
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/page2">page 2</Link>
-        </div>
+        <nav>
+            <div className="nav-wrapper">
+            <ul id="nav-mobile" className="left hide-on-med-and-down">
+            <a href="#" className="brand-logo right">
+                <i className="large material-icons">airport_shuttle</i>
+            </a>
+                 <li><Link to="/">Home</Link></li>   
+                  <li><Link to="/page2">page 2</Link></li>  
+                </ul>
+            </div>
+        </nav>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,43 @@
-import Menu from "../menu/Menu"
+import Menu from "../menu/Menu";
+import useFetch from "../hooks/useFetch";
+import "./home.scss";
 
 function Home(){
+    const url = "https://fakestoreapi.com/products?limit=5";
+    const {data, isPending, error } = useFetch(url)
+    console.log(data)
+
+    if (isPending) {
+        return <div>Chargement...</div>;
+      }
     
-    return(
-        <div>
-            <p>home</p>
-            <Menu/>
-        </div>
+      if (error) {
+        return <div>Erreur: {error}</div>;
+      }
+    
+      if (!Array.isArray(data)) {
+        return <div>Les données ne sont pas disponibles ou mal formatées.</div>;
+      }
+
+     return (
+    <div>
+      <p>home</p>
+      <Menu />
+      <div className="wrapper-home">
+        {data.length > 0 ? (
+          data.map((item) => (
+            <div key={item.id} className="card">
+              <p>{item.title}</p>
+              <img className="img-home"src={item.image} alt={item.title} />
+              <p>{item.description}</p>
+              <p>Prix: ${item.price}</p>
+            </div>   
+          ))
+        ) : (
+          <p>Aucun produit disponible</p>
+        )}
+      </div>
+    </div>
     )
 }
 
